8feb3616d334c8c3bad314cfde6f899fb86ce531
vatraai <varhegyi.tomi@gmail.com>
2020-01-31 19:35:36 +0100

    fix test coverage

--- src/components/Container/Container.test.tsx
+++ src/components/Container/Container.test.tsx
@@ -1,16 +1,17 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import Container from './Container';
 import React from 'react';
 import ProgressBar from '../ProgressBar/ProgressBar';
 
 describe('Container component', () => {
     it('should be rendered with ProgressBar', () => {
-        const wrapper = shallow(<Container />)
+        const wrapper = mount(<Container />)
         expect(wrapper.find(ProgressBar).length).toBeTruthy();
     });
 
-    it('should render snapshot', () => {
-        const wrapper = shallow(<Container />)
+    it('should render snapshot', async () => {
+        const wrapper = shallow(<Container />);
+
         expect(wrapper).toMatchSnapshot();
     });
 });
